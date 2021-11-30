675b51dad218a43d1df0edba6f380bb0a9c85c9b
Marcos Carvalho <marcosfilhosp@hotmail.com>
2021-11-30 20:37:54 -0300

    Challenge 4 Done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+// Import React Tools
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+// Import Components
+import { NotFound } from '../components';
+import CustomRender from '../CustomRender';
+
+describe('<NotFound.js /> Tests with Components', () => {
+  it('1. H2 with specific test', () => {
+    CustomRender(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('2. Image with specific adress', () => {
+    CustomRender(<NotFound />);
+    const image = screen.getByRole('img', {
+      name: /Pikachu crying/i });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
